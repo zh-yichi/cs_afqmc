fix the sign in qr

ad_afqmc/mpi_jax.py
@@ -211,6 +211,21 @@ def _prep_afqmc(options=None,option_file="options.bin",
         wave_data.update(trial_wave_data)
         wave_data["mo_coeff"] = np.eye(norb)[:,:nelec_sp[0]]
         trial = wavefunctions.ccsd_pt_ad(norb, nelec_sp, n_batch=options["n_batch"])
+    # elif options["trial"] == "ccsd_pt2_ad":
+    #     ham_data['h1_mod'] = h1_mod
+    #     amplitudes = np.load(amp_file)
+    #     t1 = jnp.array(amplitudes["t1"])
+    #     t2 = jnp.array(amplitudes["t2"])
+    #     trial_wave_data = {"t1": t1, "t2": t2}
+    #     wave_data.update(trial_wave_data)
+    #     trial = wavefunctions.ccsd_pt2_ad(norb, nelec_sp, n_batch=options["n_batch"])
+    #     nocc = nelec_sp[0]
+    #     trial_mo = trial.thouless_trans(t1)
+    #     # trial_mo = np.eye(norb)[:,:nocc]
+    #     wave_data['mo_coeff'] = trial_mo[:,:nocc]
+    #     rot_t2 = jnp.einsum('il,jk,lakb->iajb',trial_mo[:nocc,:nocc].T,
+    #                trial_mo[:nocc,:nocc].T,t2)
+    #     wave_data['rot_t2'] = rot_t2
     elif options["trial"] == "ccsd_pt2_ad":
         ham_data['h1_mod'] = h1_mod
         amplitudes = np.load(amp_file)
@@ -218,22 +233,7 @@ def _prep_afqmc(options=None,option_file="options.bin",
         t2 = jnp.array(amplitudes["t2"])
         trial_wave_data = {"t1": t1, "t2": t2}
         wave_data.update(trial_wave_data)
-        trial = wavefunctions.ccsd_pt2_ad(norb, nelec_sp, n_batch=options["n_batch"])
-        nocc = nelec_sp[0]
-        trial_mo = trial.thouless_trans(t1)
-        # trial_mo = np.eye(norb)[:,:nocc]
-        wave_data['mo_coeff'] = trial_mo[:,:nocc]
-        rot_t2 = jnp.einsum('il,jk,lakb->iajb',trial_mo[:nocc,:nocc].T,
-                   trial_mo[:nocc,:nocc].T,t2)
-        wave_data['rot_t2'] = rot_t2
-    elif options["trial"] == "ccsd_pt2_ad_smt":
-        ham_data['h1_mod'] = h1_mod
-        amplitudes = np.load(amp_file)
-        t1 = jnp.array(amplitudes["t1"])
-        t2 = jnp.array(amplitudes["t2"])
-        trial_wave_data = {"t1": t1, "t2": t2}
-        wave_data.update(trial_wave_data)
-        trial = wavefunctions.ccsd_pt2_ad_smt(
+        trial = wavefunctions.ccsd_pt2_ad(
             norb, nelec_sp, n_batch=options["n_batch"])
         nocc = nelec_sp[0]
         mo_t = trial.thouless_trans(t1)[:,:nocc]
@@ -273,13 +273,10 @@ def _prep_afqmc(options=None,option_file="options.bin",
             norb, nelec_sp, n_batch=options["n_batch"])
         noccA, noccB = trial.nelec[0], trial.nelec[1]
         wave_data["mo_coeff"] = [
-            mo_coeff[0][:, : nelec_sp[0]],
-            mo_coeff[1][:, : nelec_sp[1]],
+            mo_coeff[0][:, : noccA],
+            mo_coeff[1][:, : noccB],
         ]
-        # wave_data["mo_coeff"] = [
-        #     np.eye(norb)[:,:nocc],
-        #     ]
-        wave_data["mo_A2B"] = mo_coeff[1].T
+        wave_data["mo_A2B"] = mo_coeff[1].T # <B_p|A_q>
         ham_data['h1_mod'] = h1_mod
         amplitudes = np.load(amp_file)
         t1a = jnp.array(amplitudes["t1a"])
@@ -291,6 +288,7 @@ def _prep_afqmc(options=None,option_file="options.bin",
         mo_tb = trial.thouless_trans(t1b)[:,:noccB]
         wave_data['mo_ta'] = mo_ta
         wave_data['mo_tb'] = mo_tb
+        wave_data['mo_tb_A'] = wave_data["mo_A2B"].T @ mo_tb
         wave_data["rot_t2AA"] = jnp.einsum('ik,jl,kalb->iajb',
             mo_ta[:noccA,:noccA].T,mo_ta[:noccA,:noccA].T,t2aa)
         wave_data["rot_t2BB"] = jnp.einsum('ik,jl,kalb->iajb',

ad_afqmc/pyscf_interface.py
@@ -80,6 +80,9 @@ def prep_afqmc(
             "# If you import pyscf cc modules and use MPI for AFQMC in the same script, finalize MPI before calling the AFQMC driver."
         )
         mf = mf_or_cc._scf
+        # print(mf.mo_coeff)
+        # mo_copy = mf.mo_coeff.copy()
+        # print(mo_copy)
         cc = mf_or_cc
         if cc.frozen is not None:
             norb_frozen = cc.frozen
@@ -215,10 +218,12 @@ def prep_afqmc(
 
     # write trial mo coefficients
     trial_coeffs = np.empty((2, nbasis, nbasis))
-    overlap = mf.get_ovlp(mol)
+    overlap = mf.get_ovlp()
     if isinstance(mf, (scf.uhf.UHF, scf.rohf.ROHF)):
         uhfCoeffs = np.empty((nbasis, 2 * nbasis))
         if isinstance(mf, scf.uhf.UHF):
+            # alpha
+            print('# recovered !!')
             q, r = np.linalg.qr(
                 basis_coeff[:, norb_frozen:]
                 .T.dot(overlap)
@@ -226,15 +231,9 @@ def prep_afqmc(
             )
             sgn = np.sign(r.diagonal())
             q = np.einsum("ij,j->ij", q, sgn)
-            #
-            # q2 = basis_coeff[:, norb_frozen:].T.dot(overlap).dot(mf.mo_coeff[0][:, norb_frozen:])
-            # print("max err a", np.max(abs(q-q2)))
-            # q, _ = np.linalg.qr(
-            #    basis_coeff[:, norb_frozen:]
-            #    .T.dot(overlap)
-            #    .dot(mf.mo_coeff[0][:, norb_frozen:])
-            # )
             uhfCoeffs[:, :nbasis] = q
+            q = r = sgn = None
+            # beta
             q, r = np.linalg.qr(
                 basis_coeff[:, norb_frozen:]
                 .T.dot(overlap)
@@ -242,17 +241,20 @@ def prep_afqmc(
             )
             sgn = np.sign(r.diagonal())
             q = np.einsum("ij,j->ij", q, sgn)
-            #
-            # q2 = basis_coeff[:, norb_frozen:].T.dot(overlap).dot(mf.mo_coeff[1][:, norb_frozen:])
-            # print("max err b", np.max(abs(q-q2)))
-            # import pdb
-            # pdb.set_trace()
-            # q, _ = np.linalg.qr(
-            #     basis_coeff[:, norb_frozen:]
-            #     .T.dot(overlap)
-            #     .dot(mf.mo_coeff[1][:, norb_frozen:])
-            # )
             uhfCoeffs[:, nbasis:] = q
+            trial_coeffs[0] = uhfCoeffs[:, :nbasis]
+            trial_coeffs[1] = uhfCoeffs[:, nbasis:]
+            # print(mf.mo_coeff[0])
+            # print(mf.mo_coeff[1])
+            # print(mo_copy)
+            # mo_a = np.eye(nbasis)
+            # mo_b_A = (mo_copy[0][:, norb_frozen:].T
+            #     @ overlap
+            #     @ mo_copy[1][:, norb_frozen:])
+            # print(mo_b_A)
+            # trial_coeffs[0] = mo_a
+            # trial_coeffs[1] = mo_b_A
+            np.savez(mo_file, mo_coeff=trial_coeffs)
         else:
             q, r = np.linalg.qr(
                 basis_coeff[:, norb_frozen:]
@@ -264,10 +266,10 @@ def prep_afqmc(
             uhfCoeffs[:, :nbasis] = q
             uhfCoeffs[:, nbasis:] = q
 
-        trial_coeffs[0] = uhfCoeffs[:, :nbasis]
-        trial_coeffs[1] = uhfCoeffs[:, nbasis:]
+            trial_coeffs[0] = uhfCoeffs[:, :nbasis]
+            trial_coeffs[1] = uhfCoeffs[:, nbasis:]
         # np.savetxt("uhf.txt", uhfCoeffs)
-        np.savez(mo_file, mo_coeff=trial_coeffs)
+            np.savez(mo_file, mo_coeff=trial_coeffs)
 
     elif isinstance(mf, scf.rhf.RHF):
         q, _ = np.linalg.qr(

ad_afqmc/wavefunctions.py
@@ -3663,9 +3663,11 @@ class uccsd_pt2_ad(uhf):
         u_ji = q
         u_ai = r.T
         u_occ = jnp.vstack((u_ji,u_ai))
-        mo_t, _ = jnp.linalg.qr(u_occ ,mode='complete')
-        # sgn = jnp.sign(r.diagonal())
-        # mo_t = jnp.einsum("ij,j->ij", mo_t, sgn)
+        q, _ = jnp.linalg.qr(u_occ)# ,mode='complete')
+        sgn = jnp.sign((q).diagonal())
+        # choose the mo_t s.t it has positive olp with the original mo
+        # <psi'_i|psi_i> > 0
+        mo_t = jnp.einsum("ij,j->ij", q, sgn)
         return mo_t
     
     @partial(jit, static_argnums=0)
@@ -3676,11 +3678,11 @@ class uccsd_pt2_ad(uhf):
         wave_data: dict,
     ) -> complex:
         '''<exp(T1)HF|walker>'''
-
+        # everything in alpha basis
         # walker_dn = wave_data['mo_B'].T @ walker_dn
         olp = jnp.linalg.det(
             wave_data["mo_ta"].T.conj() @ walker_up
-        ) * jnp.linalg.det(wave_data["mo_tb"].T.conj() @ walker_dn)
+        ) * jnp.linalg.det(wave_data["mo_tb_A"].T.conj() @ walker_dn)
 
         return olp
 
@@ -3688,21 +3690,21 @@ class uccsd_pt2_ad(uhf):
     def _tls_exp1(self, x: float, h1_mod: jax.Array, walker_up: jax.Array,
                         walker_dn: jax.Array, wave_data: dict):
         '''
-        unrestricted <ep(T1)HF|exp(x*h1_mod)|walker>/<HF|walker>
+        unrestricted <ep(T1)HF|exp(x*h1_mod)|walker>
         '''
         walker_up_1x = walker_up + x * h1_mod.dot(walker_up)
         walker_dn_1x = walker_dn + x * h1_mod.dot(walker_dn)
-        walker_dn_1x = wave_data['mo_A2B'] @ walker_dn_1x
+        # walker_dn_1x = wave_data['mo_A2B'] @ walker_dn_1x
         olp = self._tls_olp(walker_up_1x, walker_dn_1x, wave_data)
-        o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
+        # o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
 
-        return olp/o0
+        return olp
 
     @partial(jit, static_argnums=0)
     def _tls_exp2(self, x: float, chol_i: jax.Array, walker_up: jax.Array,
                     walker_dn: jax.Array, wave_data: dict) -> complex:
         '''
-        <exp(T1)HF|exp(x*h2_mod)|walker>/<HF|walker>
+        <exp(T1)HF|exp(x*h2_mod)|walker>
         '''
 
         walker_up_2x = (
@@ -3715,11 +3717,11 @@ class uccsd_pt2_ad(uhf):
             + x * chol_i.dot(walker_dn)
             + x**2 / 2.0 * chol_i.dot(chol_i.dot(walker_dn))
         )
-        walker_dn_2x = wave_data['mo_A2B'] @ walker_dn_2x
+        # walker_dn_2x = wave_data['mo_A2B'] @ walker_dn_2x
         olp = self._tls_olp(walker_up_2x,walker_dn_2x,wave_data)
-        o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
+        # o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
         
-        return olp/o0
+        return olp
     
     @partial(jit, static_argnums=0)
     def _ut2_walker_olp(
@@ -3729,15 +3731,13 @@ class uccsd_pt2_ad(uhf):
         noccA, t2AA = self.nelec[0], wave_data["rot_t2AA"]
         noccB, t2BB = self.nelec[1], wave_data["rot_t2BB"]
         t2AB = wave_data["rot_t2AB"]
-        # convert trial_mo_dn into beta mo_basis
-        # wave_data["mo_B"] = <psi_a|psi_b>
-        mo_A = wave_data['mo_ta'] #@ wave_data["mo_coeff"][0]
-        mo_B = wave_data['mo_tb'] #@ wave_data["mo_B"].T @ wave_data["mo_coeff"][1]
+        mo_A = wave_data['mo_ta'] # in alpha basis
+        mo_B = wave_data['mo_tb'] # in beta basis
         green_a = (
             walker_up.dot(jnp.linalg.inv(mo_A.T.conj() @ walker_up))
         ).T
         # convert walker_dn into beta mo_basis
-        # s.t. green_b in beta basis thus can be contracted with beta amplitude
+        # s.t. green_b can be contracted with beta amplitude
         walker_dn = wave_data["mo_A2B"] @ walker_dn
         green_b = (
             walker_dn.dot(jnp.linalg.inv(mo_B.T.conj() @ walker_dn))
@@ -3756,21 +3756,21 @@ class uccsd_pt2_ad(uhf):
     def _ut2_exp1(self, x: float, h1_mod: jax.Array, walker_up: jax.Array,
                   walker_dn: jax.Array, wave_data: dict):
         '''
-        unrestricted <ep(T1)HF|T2 exp(x*h1_mod)|walker>/<HF|walker>
+        unrestricted <ep(T1)HF|T2 exp(x*h1_mod)|walker>
         '''
         walker_up_1x = walker_up + x * h1_mod.dot(walker_up)
         walker_dn_1x = walker_dn + x * h1_mod.dot(walker_dn)
         
         olp = self._ut2_walker_olp(walker_up_1x, walker_dn_1x, wave_data)
-        o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
+        # o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
 
-        return olp/o0
+        return olp
 
     @partial(jit, static_argnums=0)
     def _ut2_exp2(self, x: float, chol_i: jax.Array, walker_up: jax.Array,
                   walker_dn: jax.Array, wave_data: dict) -> complex:
         '''
-        t_ia <psi_i^a|exp(x*h2_mod)|walker>/<HF|walker>
+        t_ia <psi_i^a|exp(x*h2_mod)|walker>
         '''
 
         walker_up_2x = (
@@ -3785,9 +3785,9 @@ class uccsd_pt2_ad(uhf):
         )
         
         olp = self._ut2_walker_olp(walker_up_2x,walker_dn_2x,wave_data)
-        o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
+        # o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
         
-        return olp/o0
+        return olp
 
     @partial(jit, static_argnums=0)
     def _calc_energy_pt(self, walker_up, walker_dn, ham_data, wave_data):
@@ -3843,7 +3843,8 @@ class uccsd_pt2_ad(uhf):
 
         e1 = (d_exp1 + jnp.sum(d2_exp2) / 2.0 )
 
-        return jnp.real(t1), jnp.real(t2), jnp.real(e0), jnp.real(e1)
+        o0 = self._calc_overlap(walker_up,walker_dn,wave_data)
+        return jnp.real(t1/o0), jnp.real(t2/o0), jnp.real(e0/o0), jnp.real(e1/o0)
 
     @singledispatchmethod
     def calc_energy_pt(self, walkers, ham_data: dict, wave_data: dict) -> jax.Array: